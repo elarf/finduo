29dc7f66c61f4cbcea207f2666f4ed75d9da5335
elarf <elarf8769@gmail.com>
2026-05-26 13:08:13 +0200

    fix: Correct image path for back button in AppHeader component

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -48,7 +48,7 @@ export default function AppHeader({ onBack, rightElement }: Props) {
           onPress={handleBack}
           {...uiProps(uiPath('app_header', 'back_button', 'touchable'))}
         >
-          <Image source={require('../assets/fingo/back.png')} style={{ width: 22, height: 22 }} resizeMode="contain" />
+          <Image source={require('../../assets/fingo/back.png')} style={{ width: 22, height: 22 }} resizeMode="contain" />
         </TouchableOpacity>
       ) : (
         <TouchableOpacity
